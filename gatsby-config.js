70a45299b5f227d08a94c54051b28e2a9ca893b5
subhan300 <64373402+subhan300@users.noreply.github.com>
2021-08-20 11:29:00 +0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,13 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `xg6sud087ron`,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken:"8vszzZxAPiX_6ASAfJmCAvSB3QrGoo1Rsmze_6u9iRU"
+      }
+  ],
 }
